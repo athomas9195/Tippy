6a2069c49be72a22401d66756da339774c2ba2c7
Anna Thomas <annathomas@fb.com>
2021-06-22 12:31:58 -0700

    Initial Commit

---
+++ Tippy/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Tippy
+//
+//  Created by Anna Thomas on 6/22/21.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
